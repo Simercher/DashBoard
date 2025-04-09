32d406688abb9edd5d1d374cc04d9f0563b01fea
林煜展 <linyuzhan@linyuzhans-MacBook-Pro-3.local>
2025-04-09 20:54:27 +0800

    Start building the window from scratch

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
+use slint::ComponentHandle; // 引入 ComponentHandle 特徵
 mod ui;
-use slint::ComponentHandle;
 
 fn main() -> Result<(), slint::PlatformError> {
-    let window = ui::create_main_window();
-    window.run()
+    let _window = ui::MainWindow::new()?;
+
+    _window.on_clicked({
+        let window_handle = _window.as_weak();
+        move || {
+            if let Some(_window) = window_handle.upgrade() {
+                // 定義按鈕點擊時的行為，例如：
+                println!("按鈕被點擊了！");
+            }
+        }
+    });
+
+    _window.run()
 }
--- src/ui.rs
+++ src/ui.rs
@@ -1,7 +1,1 @@
 slint::include_modules!();
-
-pub fn create_main_window() -> MainWindow {
-    let window = MainWindow::new().unwrap();
-    window.set_battery_voltage(12.5); // 初始化電壓值
-    window
-}
